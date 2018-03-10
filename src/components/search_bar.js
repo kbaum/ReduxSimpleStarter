0760659d5c640b07b10b6af49c5fc9aacb2c560e
Karl Baum <karl.baum@gmail.com>
2018-03-10 14:19:53 -0500

    Changed to functional component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = { term: '' }
-  }
-
-  render() {
-    return (
-      <div className="search-bar">
-        <input 
-          value={this.state.term} 
-          onChange={event => this.onInputChange(event.target.value)} />
-      </div>
-    )
-  }
-
-  onInputChange(term) {
-    this.setState({term});
-    this.props.onSearchTermChange(term);
-  }
-
+const SearchBar = (props) => {
+  const onInputChange =  (term) => props.onSearchTermChange(term);
+  return ( 
+    <div className="search-bar">
+      <input 
+        onChange={event => onInputChange(event.target.value)} />
+    </div>
+  );
 }
 
 export default SearchBar;
