f8aa8630deb92013cf0a37466560a6c4e78d4e4d
Karl Baum <karl.baum@gmail.com>
2018-03-10 14:13:10 -0500

    Basic react example complete

--- src/index.js
+++ src/index.js
@@ -3,24 +3,40 @@ import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 
 const API_KEY = 'AIzaSyBQ-uv60oyWJDhkCy3CmomVpyiZcRXiRVg'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
-  
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
     return  (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail 
+          video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
